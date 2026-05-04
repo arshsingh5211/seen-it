3a573eae8aa01628d5978643df35198e65d9e380
arsh <arshsingh5211@gmail.com>
2026-05-04 12:42:49 -0400

    initial commit with boilerplate code

---
+++ src/main/java/com/arsh/seenit/SeenitApplication.java
@@ -0,0 +1,13 @@
+package com.arsh.seenit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeenitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeenitApplication.class, args);
+	}
+
+}
